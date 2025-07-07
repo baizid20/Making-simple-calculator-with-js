73c4fb921afc7b8bdd4c4d06fbef4b3711fe2d55
baizid20 <baizidul20@gmail.com>
2025-07-07 11:54:38 +0600

    operator function added

---
+++ calc.js
@@ -0,0 +1,16 @@
+// add 
+function add(a,b) {
+    return a + b
+}
+// subtract
+function sub(a,b) {
+    return a - b
+}
+// multiply
+function multiply(a,b) {
+    return a * b
+}
+// divided
+function divided(a,b) {
+    return a / b
+}
